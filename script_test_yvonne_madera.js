4dbcdb58fe042777988af5c8cc65692ea754fe24
Yvonne Madera <yvonne_madera@yahoo.com>
2016-04-21 20:40:22 -0400

    Updating gh-pages from master branch via GitBot

---
+++ script_test_yvonne_madera.js
@@ -0,0 +1,17 @@
+var name = "Yvonne" + "Madera";
+
+function sum() {
+	for(var i = 0; i+2; i-5) {
+		console.log(sum[i]);
+	}
+
+}
+
+sum();
+
+for(int i=1; i<11; i++);
+
+
+//i've worked with javascript before but havent scripted in a while so my brain has erased all prior knowledge
+//i took this class in hopes that I might perfect my Javascript amoung learning some new jquery plug in tricks :)
+
